c1c42b7323c09873bbc6d6342265d691f6db7d87
廖睿 <liaorui@curefun.com>
2023-03-23 14:33:04 +0800

    修改chat类

--- app/Logic/Utils/Chat.php
+++ app/Logic/Utils/Chat.php
@@ -9,28 +9,68 @@
 namespace App\Logic\Utils;
 
 
+use App\Model\Live\ChatLogsModel;
+
 class Chat
 {
 
-    private $_OPENAI_API_KEY='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';
-    private $_url="https://api.openai.com/v1/completions";
+    private $_OPENAI_API_KEY;
+//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
+    private $_url="https://api.openai.com/v1/chat/completions";
+    private $_data=[];
     /**
      * Chat constructor.
      */
     public function __construct(){
-
+        $this->_OPENAI_API_KEY = env('CHAT_KEY','');
+        $this->_data = [
+            "model" => "gpt-3.5-turbo",
+            "temperature"=>0,
+            "presence_penalty"=>0,
+            "frequency_penalty"=>0,
+            "top_p"=>1,
+            "max_tokens"=>2048,
+//            "stream"=>true
+        ];
     }
 
+    public function Message($prompt){
+        $this->_data['messages'] = array(
+            array(
+                "role" => "user",
+                "content" => $prompt
+            )
+        );
+        return $this;
+    }
+
+    public function Messages($prompt,$role='user'){
+        if(isset($this->_data['messages'])){
+            array_push($this->_data['messages'],array(
+                "role" => $role,
+                "content" => $prompt
+            ));
+        } else {
+            $this->_data['messages'] = array(
+                array(
+                    "role" => $role,
+                    "content" => $prompt
+                )
+            );
+        }
+        return $this;
+    }
     public function liaotian(){
-
+        return $this->curl($this->_url,$this->_data);
     }
 
-    private  function curl($data){
+    private  function curl($url,$data){
+        $stime=microtime(true);
         $headers = array();
         $headers[] = "Content-Type: application/json";
         $headers[] = "Authorization: Bearer ".$this->_OPENAI_API_KEY;
         $options = array(
-            CURLOPT_URL => "https://api.openai.com/v1/completions",
+            CURLOPT_URL => $url,
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => json_encode($data),
             CURLOPT_HTTPHEADER => $headers,
@@ -42,10 +82,35 @@ class Chat
         curl_setopt_array($curl, $options);
 
         $response = curl_exec($curl);
+        $etime=microtime(true);//获取程序执行结束的时间
+        $total=$etime-$stime;   //计算差值
         if (curl_errno($curl)) {
             echo "Error: " . curl_error($curl);
         } else {
-            var_dump($response);
+            if(isset($response['error'])){
+                echo "Error: " . $response['message'];
+            }
+            $response = json_decode($response,true);
         }
+        $id = $this->write($data,$response,$total);
+        $response['id'] = $id;
+        return $response;
+    }
+
+    public function write($reuqest,$response,$total){
+
+        $insert = [
+            'request'=>json_encode($reuqest),
+            'request_len'=>strlen(json_encode($reuqest)),
+            'response'=>json_encode($response),
+            'response_len'=>strlen(json_encode($response)),
+            'execution_time'=>$total,
+        ];
+
+        if($response){
+            $insert['prompt_tokens']=$response['usage']['prompt_tokens'];
+            $insert['completion_tokens']=$response['usage']['completion_tokens'];
+        }
+        return ChatLogsModel::m()->insertGetId($insert);
     }
 }
